4f7003a8a4b862086d66ce3302929e98c5ca617e
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-07-18 12:07:24 +0100

    no message

--- force_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# bash git update script
-
-git fetch --all
-
-git reset --hard origin/master
--- scripts/nginx_stop.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-(( UID != 0 )) && { echo "Error: you MUST be logged in as root."; exit 1; }
-
-echo "Stopped NGINX + RTMP"
-
-/usr/local/nginx/sbin/nginx -s stop
-
-echo "Done"
